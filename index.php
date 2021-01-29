0c5be0a40fe5637830a5e64705d6c3918f0fbd71
Muhammad Taufiq <56469414+MuhammadTaufiq01@users.noreply.github.com>
2021-01-30 03:18:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -75,9 +75,10 @@ if (isset($_POST["submit"])) {
 			<?php endif; ?>
 
 			<!-- button -->
-			<a href="home.php" type="submit" name="submit" class="btn-login">
+			<a class="btn btn-login" href="home.php" role="button" name="submit" style="text-decoration:none;">Login</a>
+			<!-- <button href="home.php" type="submit" name="submit" class="btn-login">
 				Login
-			</a>
+			</button> -->
 		</div>
 
 		<br>
